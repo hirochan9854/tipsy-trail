08eae07b2ab79fcf2c5bbad971b5f34a04641dc7
okayu <146696282+okayama0709@users.noreply.github.com>
2025-01-15 10:38:51 +0900

    コンポーネントの読み込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Footer } from '@/components/Footer';
-import { BackButton } from '@/components/BackButton';
-import { SearchButton } from '@/components/SearchButton';
 
 export const metadata: Metadata = {
   title: 'Tipsy Trail',
@@ -17,7 +15,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className="bg-[#0C151B] relative z-0">
         <h1 className="text-lg font-bold text-[#D6D1BF] text-center  mt-[50px]">TipsyTrail</h1>
-        <SearchButton />
+
         {children}
         <Footer />
       </body>
