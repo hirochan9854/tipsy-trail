815e1df81367c2eddd2dbb381b564473f6f10e04
hirochan9854 <ito.hiro9854@gmail.com>
2025-01-15 11:04:03 +0900

    トップページ作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
 import { AlcoholListItem } from '@/components/AlcoholListItem';
 import { RecommendedModal } from '@/components/RecommendedModal';
 
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
+    <div className="px-[15px] pt-[52px] ">
       <RecommendedModal />
-      <AlcoholListItem />
+      <div className="mb-12">
+        <div className="mb-3">
+          <h3 className="text-[#D6D1BF] mb-1 ml-5">あなたへのおすすめ</h3>
+          <Image src={'/deco-white-line.svg'} width={300} height={7} alt="" />
+        </div>
+        <div className="flex overflow-scroll gap-6 flex-nowrap w-screen">
+          {[...Array(10)].map((_, i) => (
+            <AlcoholListItem key={i} />
+          ))}
+        </div>
+      </div>
+      <div className="mb-12">
+        <div className="mb-3">
+          <h3 className="text-[#D6D1BF] mb-1 ml-5">しっぽり</h3>
+          <Image src={'/deco-white-line.svg'} width={300} height={7} alt="" />
+        </div>
+        <div className="flex overflow-scroll gap-6 flex-nowrap w-screen">
+          {[...Array(10)].map((_, i) => (
+            <AlcoholListItem key={i} />
+          ))}
+        </div>
+      </div>
+      <div className="mb-12">
+        <div className="mb-3">
+          <h3 className="text-[#D6D1BF] mb-1 ml-5">楽しむ</h3>
+          <Image src={'/deco-white-line.svg'} width={300} height={7} alt="" />
+        </div>
+        <div className="flex overflow-scroll gap-6 flex-nowrap w-screen">
+          {[...Array(10)].map((_, i) => (
+            <AlcoholListItem key={i} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/AlcoholListItem.tsx
+++ src/components/AlcoholListItem.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const AlcoholListItem: React.FC = () => {
   return (
-    <Link href={'/alcohol/1'} className="text-white flex flex-col items-center w-20">
+    <Link href={'/alcohol/1'} className="text-white flex flex-col items-center min-w-20 ">
       <Image src={'/img-sample-sake.png'} width={76} height={85} alt="" />
       <h3 className="text-[13px] font-normal">かぼすハイ</h3>
       <p className="text-[10px] ">度数:50%</p>
