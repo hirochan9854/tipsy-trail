228cac89fa8d296d84e3534bd6e8cc5570ed02fb
okayu <146696282+okayama0709@users.noreply.github.com>
2025-01-15 10:18:37 +0900

    必要データ追加

--- src/mock/mockAlcohols.ts
+++ src/mock/mockAlcohols.ts
@@ -5,7 +5,8 @@ export const mockAlcohols: alcohols[] = [
     id: 1,
     name: 'かぼすハイボール',
     ename: 'Kabosu Highball',
-    description: 'かぼすの爽やかな香りが楽しめるハイボール',
+    description:
+      'かぼすの爽やかな香りが楽しめるハイボール。新鮮なかぼすの果汁を使用し、爽やかな酸味とほのかな甘みが特徴です。リフレッシュしたい時や食事と一緒に楽しむのに最適です。',
     base: 'ハイボール',
     alcoholContent: '50%',
     feature: 'さっぱり',
